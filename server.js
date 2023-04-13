c8ee6763de544798d279f244a132d48a1e5ae710
yushanwebdev <yushanwebdev@gmail.com>
2023-04-13 22:11:17 +0530

    feat: Develop a Caching Strategy

--- server.js
+++ server.js
@@ -4,6 +4,43 @@ const redis = require("redis");
 const client = redis.createClient();
 
 const rIncr = promisify(client.incr).bind(client);
+const rGet = promisify(client.get).bind(client);
+const rSetex = promisify(client.setex).bind(client);
+
+async function verySlowAndExpensivePostgresSQLQuery() {
+  // here you would do a big ugly query for PostgreSQL
+  console.log(`oh no a very expensive query`);
+
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(new Date().toUTCString());
+    }, [5000]);
+  });
+
+  return promise;
+}
+
+function cache(key, ttl, slowFn) {
+  return async function cachedFn(...props) {
+    const cachedResponse = await rGet(key);
+
+    if (cachedResponse) {
+      console.log(`hooray it is cached`);
+
+      return cachedResponse;
+    }
+
+    const result = await slowFn(...props);
+    await rSetex(key, ttl, result);
+    return result;
+  };
+}
+
+const cachedFn = cache(
+  "expensive_call",
+  10,
+  verySlowAndExpensivePostgresSQLQuery
+);
 
 async function init() {
   const app = express();
@@ -14,6 +51,15 @@ async function init() {
     res.json({
       status: "ok",
       views,
+    });
+  });
+
+  app.get("/cache", async (req, res) => {
+    const data = await cachedFn();
+
+    res.json({
+      status: "ok",
+      data,
     });
   });
 
